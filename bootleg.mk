a165cad7d274e0845f94e8f80618e174a383936c
merothh <merothh@gmail.com>
2018-04-03 21:07:18 +0530

    tissot: we are shishufied

--- bootleg.mk
+++ bootleg.mk
@@ -26,7 +26,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys" \
-	DEVICE_MAINTAINERS="IamJoker03"    
+	DEVICE_MAINTAINERS="IamJoker03"
+
+# Set BTLG Build to Shishufied
+BOOTLEG_BUILD_TYPE := Shishufied
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
